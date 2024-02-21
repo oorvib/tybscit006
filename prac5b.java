9925e88b36c4900b0214d8bdf18195c8a89eab7a
oorvi <balaoorvi@gmail.com>
2024-02-21 13:51:15 +0530

    Code updated!

--- prac5b.java
+++ prac5b.java
@@ -1,9 +1,10 @@
 public class prac5b {
-    // Burhanuddin Khairulla TYBSCIT 036
+
     public static void main(String[] args) {
         int number = 5; 
         long factorial = calculateFactorial(number);
         System.out.println("Factorial of " + number + " is: " + factorial);
+	System.out.println("Oorvi Bala");
     }
 
     public static long calculateFactorial(int n) {
